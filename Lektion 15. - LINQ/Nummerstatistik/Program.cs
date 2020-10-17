53f017708bdf2715b0b445dcdf15dfe148515870
Warchiefen <70097003+Warchiefen@users.noreply.github.com>
2020-10-17 15:34:44 +0200

    LINQ-Method-Syntax

---
+++ Lektion 15. - LINQ/Nummerstatistik/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nummerstatistik
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            var numbers = new List<int>();
+
+            Console.WriteLine("mata in siffror, tryck en blank enter för att avsluta.");
+
+            bool done = true;
+            while (done)
+            {
+                var input = Console.ReadLine();
+                if (input == "")
+                {
+                    done = false;
+                }
+                else
+                {
+                    var num = int.Parse(input);
+                    numbers.Add(num);
+                }
+            }
+
+            var sumNumbers = numbers.Sum(); //Sum of numbers 
+            Console.WriteLine("Sum: " + sumNumbers);
+
+            var largestNumbers = numbers.Max(); //Largest numbers
+            Console.WriteLine("Largest: " + largestNumbers);
+
+            var smallestNumbers = numbers.Min(); //Smallest numbers
+            Console.WriteLine("Smallest: " + smallestNumbers);
+
+            var averageNumbers = numbers.Average(); //Average number
+            Console.WriteLine("Average: " + averageNumbers);
+
+            var tenOrHigher = numbers.Where(x => x >= 10); //10 or higher in numbers
+            Console.Write("10 or higher: ");
+            Console.WriteLine(string.Join(", ", tenOrHigher));
+
+            var betweenTenAndTwenty = numbers.Where(x => x >= 10 && x <= 20); //Between 10 and 20 in numbers
+            Console.Write("Between 10 and 20: ");
+            Console.WriteLine(string.Join(", ", betweenTenAndTwenty));
+
+            var sumBetweeenTenAndTwenty = numbers.Where(x => x >= 10 && x <= 20).Sum(); //Sum of numbers between 10 and 20 in numbers
+            Console.WriteLine("Sum between 10 and 20: " + sumBetweeenTenAndTwenty);
+
+            var squares = numbers.Select(x => x * x); //Squares of numbers in numbers
+            Console.Write("Squares: ");
+            Console.WriteLine(string.Join(", ", squares));
+
+            var orderBy = numbers.OrderBy(x => x); //Ordering numbers in numbers
+            Console.Write("Ordered: ");
+            Console.WriteLine(string.Join(", ", orderBy));
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            using FakeConsole console = new FakeConsole("First input", "Second input");
+            Program.Main();
+            Assert.AreEqual("Hello!", console.Output);
+        }
+    }
+}
---
+++ Lektion 15. - LINQ/Personstatistik/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Personstatistik
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public double Weight;
+        public double Height;
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            var personList = new List<Person>();
+
+            while (true)
+            {
+                Console.Write("Name (Press blank Enter to Exit): ");
+                var name = Console.ReadLine();
+                if (name == "")
+                {
+                    break; //jump out of the loop.
+                }
+                Console.Write("Age: ");
+                var age = int.Parse(Console.ReadLine());
+                Console.Write("Weight: ");
+                var weight = double.Parse(Console.ReadLine());
+                Console.Write("Height: ");
+                var height = double.Parse(Console.ReadLine());
+                Console.Clear();
+
+                var p = new Person
+                {
+                    Name = name,
+                    Age = age,
+                    Weight = weight,
+                    Height = height
+                };
+
+                personList.Add(p);
+            }
+            Console.WriteLine("Thank you for your inputs.");
+            Console.WriteLine();
+
+            var averageAge = personList.Average(p => p.Age);
+            Console.WriteLine("Average: " + averageAge);
+
+            var tallestPerson = personList.OrderByDescending(p => p.Height).Select(tallestPerson => tallestPerson.Name + ": " + tallestPerson.Height).First(); //Få in namnet på personen
+            Console.WriteLine("Tallest person: " + tallestPerson);
+
+            var highestBM = personList.OrderByDescending(p => p.Weight / (p.Height * p.Height)).Select(p => p.Name + ": " + p.Weight / (p.Height * p.Height)).First();
+            Console.WriteLine("Highest BMI: " + highestBM);
+
+            var tallerThan180 = personList.Where(p => p.Height >= 1.80).Select(p => p.Name + ": " + p.Height);
+            Console.Write("1.80 or taller: ");
+            Console.WriteLine(string.Join(", ", tallerThan180));
+
+            var firstLetters = personList.Select(p => p.Name.First());
+            Console.Write("First letters: ");
+            Console.WriteLine(string.Join(", ", firstLetters));
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            using FakeConsole console = new FakeConsole("First input", "Second input");
+            Program.Main();
+            Assert.AreEqual("Hello!", console.Output);
+        }
+    }
+}
